9d06a21d2c097abc77f644e84a6f910d72c699d9
Sanju P Saji <sanjusaji91@gmail.com>
2023-03-26 18:29:33 +0530

    Start page creation

---
+++ app/src/main/java/com/onthelookout/otl/Start.java
@@ -0,0 +1,41 @@
+package com.onthelookout.otl;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Start extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        //Set the title
+        getSupportActionBar().setTitle("On The Lookout");
+
+        //Open login Activity
+        Button buttonLogin = findViewById(R.id.button_login);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Start.this, Login.class);
+                startActivity(intent);
+            }
+        });
+
+        Button buttonRegister = findViewById(R.id.button_register);
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Start.this, Register.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
