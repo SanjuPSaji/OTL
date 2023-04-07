8499583ceaa3dee6b8b8fdec15b37a578be29788
Sankalps-world <sankalptaneja1313@gmail.com>
2023-04-07 22:17:01 +0530

    Redirect upload to complaint page and store complaint in firebase

--- app/src/main/java/com/onthelookout/otl/Complaint.java
+++ app/src/main/java/com/onthelookout/otl/Complaint.java
@@ -1,13 +1,42 @@
 package com.onthelookout.otl;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.FirebaseDatabase;
+import com.onthelookout.otl.ui.home.HomeFragment;
+
+import java.util.HashMap;
 
 public class Complaint extends AppCompatActivity {
-
+EditText info_complaint;
+Button submit_complaint;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_complaint);
+
+        info_complaint = findViewById(R.id.info_complaint);
+        submit_complaint = findViewById(R.id.submit_complaint);
+
+        String type = getIntent().getStringExtra("type");
+        submit_complaint.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                HashMap<String ,Object> m = new HashMap<String, Object>();
+                m.put("Information",info_complaint.getText().toString());
+                m.put("Type",type);
+                FirebaseDatabase.getInstance().getReference().child("Complaints").push().setValue(m);
+
+                Intent intent = new Intent(Complaint.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/onthelookout/otl/ui/upload/UploadFragment.java
+++ app/src/main/java/com/onthelookout/otl/ui/upload/UploadFragment.java
@@ -1,23 +1,31 @@
 package com.onthelookout.otl.ui.upload;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.onthelookout.otl.Complaint;
+import com.onthelookout.otl.R;
+
 import com.onthelookout.otl.databinding.FragmentUploadBinding;
 
 public class UploadFragment extends Fragment {
-
+    Activity context;
     private FragmentUploadBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+        context = getActivity();
+
         UploadViewModel uploadViewModel =
                 new ViewModelProvider(this).get(UploadViewModel.class);
 
@@ -27,6 +35,31 @@ public class UploadFragment extends Fragment {
         final TextView textView = binding.textUpload;
         uploadViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
+
+
+
+    }
+    public void onStart(){
+        super.onStart();
+        Button buttonfire = (Button) context.findViewById(R.id.button_fire);
+        Button buttonearthquake = (Button) context.findViewById(R.id.button_earthquake);
+        buttonfire.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, Complaint.class);
+                intent.putExtra("type","Fire");
+                startActivity(intent);
+            }
+        });
+
+        buttonearthquake.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, Complaint.class);
+                intent.putExtra("type","Earthquake");
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
